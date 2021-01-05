ed7bf24ae696d66c442f51b5f94f1421ad81d11c
HJungIn <gjwjddls96@naver.com>
2021-01-05 17:38:25 +0900

    버튼 5 : ...rest props 전달하기

--- Component-스타일링/styling-with-sass/src/App.js
+++ Component-스타일링/styling-with-sass/src/App.js
@@ -35,6 +35,18 @@ function App() {
         <Button color="gray" fullWidth>Button</Button>
         <Button color="pink" size="small" fullWidth>Button</Button>
       </div>
+
+      {/* ...rest를 이용한 값 전달 */}
+      <div className="buttons">
+        <Button 
+        color="blue" 
+        size="large" 
+        fullWidth onClick={() => console.log('클릭됐다!')}  //...rest로 들어감
+        onMouseMove={()=>{console.log('마우스무브!');}} //...rest로 들어감
+        >
+          Button!!!
+        </Button> 
+      </div>
     </div>
   );
 }
--- Component-스타일링/styling-with-sass/src/components/Button.js
+++ Component-스타일링/styling-with-sass/src/components/Button.js
@@ -3,13 +3,18 @@ import './Button.scss';
 import classnames from 'classnames';
 
 //size : large, medium, small
-function Button({ children, size, color, outline, fullWidth }) {
+function Button({ children, size, color, outline, fullWidth, className, ...rest }) { // 앞의 파라미터를 뺀 나머지 파라미터(함수포함)들의 모임
   // return <button className={['Button', size].join(' ')}>{children}</button>; //Button size 형식으로 나옴 => 내가 다 써줘야함
   // return <button className={`Button ${size}`}>{children}</button>; //템플릿 형식 => 내가 다 써줘야함
   return <button className={classnames('Button', size, color, {
     outline, //true일 때만 className에 추가 된다.
     fullWidth //true일 때만 className에 추가 된다.
-  })}>{children}</button>; //classnames 라이브러리를 이용해 자동으로 정렬되도록 해준다
+  }, className //선택형으로 커스텀하는것을 원할 때 추가
+  )}
+  
+  {...rest} //rest객체 안에 있는 모든 것들을 이 버튼에게 설정해준다. (onClick, mousemove..등등)
+
+  >{children}</button>; //classnames 라이브러리를 이용해 자동으로 정렬되도록 해준다
 }
 
 Button.defaultProps = { //기본값

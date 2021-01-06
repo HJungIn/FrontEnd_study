4150d241d3c802d94861b660162f7e9e86624c0e
HJungIn <gjwjddls96@naver.com>
2021-01-06 17:12:09 +0900

    styled-components의 기초 : 원만들기

--- Component-스타일링/styling-with-styled-components/src/App.js
+++ Component-스타일링/styling-with-styled-components/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { css } from 'styled-components'; //태그 템플릿을 사용하기 위해 css를 불러온다
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color || 'black'};
+  border-radius: 50%;
+  ${props => //여러줄의 props를 적용하기
+    props.huge &&
+    css` //여러 줄의 CSS 코드를 조건부로 보여주고 싶다면 css 를 사용 => css 를 불러와서 사용을 해야 그 스타일 내부에서도 다른 props 를 조회 할 수 있다.
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Circle color="black" />
+      <Circle color="red" huge />
+    </>
   );
+    
 }
 
 export default App;

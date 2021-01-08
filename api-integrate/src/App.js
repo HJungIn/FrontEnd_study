e9cb564d823c637b8269fd79777609a4c923d63a
HJungIn <gjwjddls96@naver.com>
2021-01-08 14:34:43 +0900

    API 연동 방법 2 : useReducer 로 요청 상태 관리하기

--- api-integrate/src/App.js
+++ api-integrate/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Users from './User';
+import UsersUseReduce from './UsersUseReduce';
 
 function App() {
   return (
-    <Users />
+    <>
+      {/* useState와 useEffect로 데이터 로딩하기 */}
+      <Users />
+
+      {/* useReducer 로 요청 상태 관리하기 */}
+      <UsersUseReduce />
+    </>
   );
 }
 

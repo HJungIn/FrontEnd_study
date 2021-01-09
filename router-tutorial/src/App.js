59baf018bce20376a6905a00cc06184cdeb94423
HJungIn <gjwjddls96@naver.com>
2021-01-09 16:29:25 +0900

    리액트 라우터 기본 1 : 기본적인 사용법

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom'; // Route : 특정 주소에 특정 컴포넌트를 보여주겠다. || Link : 특정 링크를 눌렀을 때 다른 경로로 가고싶을 때 사용하는 컴포넌트 (a태그 사용x)
+import About from './About';
+import Home from './Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <>
+      {/* 특정 주소에 따라 다른 결과물을 보여주는 방법 */}
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+
+      <hr />
+      
+      <Route path="/" component={Home} exact /> {/* exact : 경로가 완전히 /로 일치할 때만 보여주겠다! 라는 의미 */}
+      <Route path="/about" component={About} />
+      </>
     </div>
   );
-}
+};
 
 export default App;
--- router-tutorial/src/index.js
+++ router-tutorial/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'; // * BrowserRouter 불러오기 : History API사용
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter> {/* 프로젝트에 라우터 적용 */}
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 

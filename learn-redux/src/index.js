f7dd2482c397fc2d8a6c5f3975c82085e85503f9
HJungIn <gjwjddls96@naver.com>
2021-01-12 16:52:30 +0900

    리덕스 4 : 리덕스 개발자 도구 적용하기

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -6,8 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux'; // Provider을 통해 리액트 프로젝트에서 리덕스 사용 가능
 import {createStore} from 'redux';
 import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer); // 스토어를 만듭니다.
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다. || composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
 console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
 
 ReactDOM.render(

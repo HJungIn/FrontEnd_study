2da46f93fda975202797bef09f92b1afceeb0ea0
HJungIn <gjwjddls96@naver.com>
2021-01-14 17:43:02 +0900

    리덕스 미들웨어 3-3 : redux-thunk로 Promise - createPromiseThunk

--- learn-redux-middleware/src/lib/asyncUtils.js
+++ learn-redux-middleware/src/lib/asyncUtils.js
@@ -1,3 +1,25 @@
+// createPromiseThunk : Promise에 기반한 Thunk를 만들어주는 함수입니다.
+export const createPromiseThunk = (type, promiseCreator) => { // type : 요청들에 대해 요청이 시작했음을 알려주는 타입 (GET_POSTS) || promiseCreator : postsAPI.getPosts()처럼 Promise를 만들어주는 함수
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  
+    /* thunk 함수를 만들기 */
+    // 이 함수는 promiseCreator가 단 하나의 파라미터만 받는다는 전제하에 작성되었습니다.
+    // 만약 여러 종류의 파라미터를 전달해야하는 상황에서는 객체 타입의 파라미터를 받아오도록 하면 됩니다.
+    // 예: writeComment({ postId: 1, text: '댓글 내용' });
+    // FSA(Flux Standard Action) 규칙 : payload로 통일, error시 error를 true로 변환
+    return param => async dispatch => {
+      // 요청 시작
+      dispatch({ type, param }); //로딩 상태 활성화
+      try {
+        const payload = await promiseCreator(param);// 결과물의 이름을 payload 라는 이름으로 통일한다. posts,post등 이름이 다르면 최적화가 어려워지기 때문에
+        dispatch({ type: SUCCESS, payload }); // 성공
+      } catch (e) {
+        dispatch({ type: ERROR, payload: e, error: true }); // 실패
+      }
+    };
+  };
+  
+  
   // 리듀서에서 사용 할 수 있는 여러 유틸 함수들입니다.
   export const reducerUtils = {
     // 초기 상태. 초기 data 값은 기본적으로 null 이지만 바꿀 수도 있습니다.
@@ -13,9 +35,9 @@
       error: null
     }),
     // 성공 상태
-    success: data => ({
+    success: payload => ({
       loading: false,
-      data,
+      data: payload,
       error: null
     }),
     // 실패 상태
--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -13,28 +13,33 @@ const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
 /* 2. 각 프로미스마다 thunk 함수를 만들어주어야 합니다 */
-// thunk 를 사용 할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없습니다.
-// 그냥 thunk 함수에서 바로 액션 객체를 만들어주어도 괜찮습니다.
-export const getPosts = () => async dispatch => { // asycn await 사용
-  dispatch({ type: GET_POSTS }); // 요청이 시작됨
-  try {
-    const posts = await postsAPI.getPosts(); // API 호출
-    dispatch({ type: GET_POSTS_SUCCESS, posts }); // 성공
-  } catch (e) {
-    dispatch({ type: GET_POSTS_ERROR, error: e }); // 실패
-  }
-};
+// // thunk 를 사용 할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없습니다.
+// // 그냥 thunk 함수에서 바로 액션 객체를 만들어주어도 괜찮습니다.
+// export const getPosts = () => async dispatch => { // asycn await 사용
+//   dispatch({ type: GET_POSTS }); // 요청이 시작됨
+//   try {
+//     const posts = await postsAPI.getPosts(); // API 호출
+//     dispatch({ type: GET_POSTS_SUCCESS, posts }); // 성공
+//   } catch (e) {
+//     dispatch({ type: GET_POSTS_ERROR, error: e }); // 실패
+//   }
+// };
 
-// thunk 함수에서도 파라미터를 받아와서 사용 할 수 있습니다.
-export const getPost = id => async dispatch => {
-  dispatch({ type: GET_POST }); // 요청이 시작됨
-  try {
-    const post = await postsAPI.getPostById(id); // API 호출
-    dispatch({ type: GET_POST_SUCCESS, post }); // 성공
-  } catch (e) {
-    dispatch({ type: GET_POST_ERROR, error: e }); // 실패
-  }
-};
+// // thunk 함수에서도 파라미터를 받아와서 사용 할 수 있습니다.
+// export const getPost = id => async dispatch => {
+//   dispatch({ type: GET_POST }); // 요청이 시작됨
+//   try {
+//     const post = await postsAPI.getPostById(id); // API 호출
+//     dispatch({ type: GET_POST_SUCCESS, post }); // 성공
+//   } catch (e) {
+//     dispatch({ type: GET_POST_ERROR, error: e }); // 실패
+//   }
+// };
+
+//createPromiseThunk 사용해서 thunk함수 만들기 -> 최적화 한 것
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+
 
 const initialState = {
 //   posts: {
@@ -123,12 +128,12 @@ export default function posts(state = initialState, action) {
     case GET_POSTS_SUCCESS:
       return {
         ...state,
-        posts: reducerUtils.success(action.posts)
+        posts: reducerUtils.success(action.payload)
       };
     case GET_POSTS_ERROR:
       return {
         ...state,
-        posts: reducerUtils.error(action.error)
+        posts: reducerUtils.error(action.payload)
       };
     case GET_POST:
       return {
@@ -138,12 +143,12 @@ export default function posts(state = initialState, action) {
     case GET_POST_SUCCESS:
       return {
         ...state,
-        post: reducerUtils.success(action.post)
+        post: reducerUtils.success(action.payload)
       };
     case GET_POST_ERROR:
       return {
         ...state,
-        post: reducerUtils.error(action.error)
+        post: reducerUtils.error(action.payload)
       };
     default:
       return state;

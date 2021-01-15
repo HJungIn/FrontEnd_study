0d9beb8d5acfacb6565ab219036cbf937d966589
HJungIn <gjwjddls96@naver.com>
2021-01-15 17:23:19 +0900

    리덕스 미들웨어 6 : JSON Server

--- learn-redux-middleware/src/api/posts.js
+++ learn-redux-middleware/src/api/posts.js
@@ -1,3 +1,4 @@
+/*
 // n 밀리세컨드동안 기다리는 프로미스를 만들어주는 함수
 const sleep = n => new Promise(resolve => setTimeout(resolve, n));
 
@@ -31,4 +32,19 @@ export const getPosts = async () => {
 export const getPostById = async id => {
   await sleep(500); // 0.5초 쉬고
   return posts.find(post => post.id === id); // id 로 찾아서 반환
+};
+*/
+
+import axios from 'axios'; //axios 사용시
+
+// 포스트 목록을 가져오는 비동기 함수
+export const getPosts = async () => {
+  const response = await axios.get('http://localhost:4000/posts');
+  return response.data;
+};
+
+// ID로 포스트를 조회하는 비동기 함수
+export const getPostById = async id => {
+  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  return response.data;
 };

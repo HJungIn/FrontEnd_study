0fb40451f70851b7a8cef023aeb643eb29c3b971
HJungIn <gjwjddls96@naver.com>
2021-01-20 23:33:17 +0900

    타입스크립트 5-3 : 타입스크립트와 리덕스 - typesafe-actions 로 리팩토링

--- typescript-react-redux-tutorial/src/modules/counter.ts
+++ typescript-react-redux-tutorial/src/modules/counter.ts
@@ -1,28 +1,37 @@
+import { ActionType, createReducer, deprecated } from 'typesafe-actions';
+
 // 1. 액션 타입을 선언합니다
 // 뒤에 as const 를 붙여줌으로써 나중에 액션 객체를 만들게 action.type 의 값을 추론하는 과정에서
 // action.type 이 string 으로 추론되지 않고 'counter/INCREASE' 와 같이 실제 문자열 값으로 추론 되도록 해줍니다.
-const INCREASE = 'counter/INCREASE' as const;
-const DECREASE = 'counter/DECREASE' as const;
-const INCREASE_BY = 'counter/INCREASE_BY' as const;
+// const INCREASE = 'counter/INCREASE' as const;
+// const DECREASE = 'counter/DECREASE' as const;
+// const INCREASE_BY = 'counter/INCREASE_BY' as const;
+const INCREASE = 'counter/INCREASE'; //리팩토링
+const DECREASE = 'counter/DECREASE';
+const INCREASE_BY = 'counter/INCREASE_BY';
 
 // 2. 액션 생성함수를 선언합니다
-export const increase = () => ({
-  type: INCREASE
-});
+// export const increase = () => ({
+//   type: INCREASE
+// });
 
-export const decrease = () => ({
-  type: DECREASE
-});
+// export const decrease = () => ({
+//   type: DECREASE
+// });
 
-export const increaseBy = (diff: number) => ({
-  type: INCREASE_BY,
-  // 액션에 부가적으로 필요한 값을 payload 라는 이름으로 통일합니다
-  // 이는 FSA (https://github.com/redux-utilities/flux-standard-action) 라는 규칙인데
-  // 이 규칙을 적용하면 액션들이 모두 비슷한 구조로 이루어져있게 되어 추후 다룰 때도 편하고
-  // 읽기 쉽고, 액션 구조를 일반화함으로써 액션에 관련돤 라이브러리를 사용 할 수 있게 해줍니다.
-  // 다만, 무조건 꼭 따를 필요는 없습니다.
-  payload: diff //액션의 모양새를 통일시킴으로써 개발에 편의성을 준다.
-});
+// export const increaseBy = (diff: number) => ({
+//   type: INCREASE_BY,
+//   // 액션에 부가적으로 필요한 값을 payload 라는 이름으로 통일합니다
+//   // 이는 FSA (https://github.com/redux-utilities/flux-standard-action) 라는 규칙인데
+//   // 이 규칙을 적용하면 액션들이 모두 비슷한 구조로 이루어져있게 되어 추후 다룰 때도 편하고
+//   // 읽기 쉽고, 액션 구조를 일반화함으로써 액션에 관련돤 라이브러리를 사용 할 수 있게 해줍니다.
+//   // 다만, 무조건 꼭 따를 필요는 없습니다.
+//   payload: diff //액션의 모양새를 통일시킴으로써 개발에 편의성을 준다.
+// });
+
+export const increase = deprecated.createStandardAction(INCREASE)(); //리팩토링
+export const decrease = deprecated.createStandardAction(DECREASE)();
+export const increaseBy = deprecated.createStandardAction(INCREASE_BY)<number>(); // payload 타입을 Generics 로 설정해주세요.
 
 
 type CounterState = {// 이 리덕스 모듈에서 관리 할 상태의 타입을 선언합니다 => 상태에 대한 type 생성해야함
@@ -36,28 +45,52 @@ const initialState: CounterState = {
 
 // 모든 액션 겍체들에 대한 타입을 준비해줍니다.
 // 상단부에서 액션타입을 선언 할 떄 as const 를 하지 않으면 이 부분이 제대로 작동하지 않습니다.
-type CounterAction =
-  | ReturnType<typeof increase> // ReturnType<typeof ___함수명__> 는 특정 함수의 반환값을 추론해줍니다
-  | ReturnType<typeof decrease>
-  | ReturnType<typeof increaseBy>;
+// type CounterAction =
+//   | ReturnType<typeof increase> // ReturnType<typeof ___함수명__> 는 특정 함수의 반환값을 추론해줍니다
+//   | ReturnType<typeof decrease>
+//   | ReturnType<typeof increaseBy>;
+
+// 액션 객체 타입 준비 => 리팩토링
+const actions = { increase, decrease, increaseBy }; // 모든 액션 생성함수들을 actions 객체에 넣습니다
+type CounterAction = ActionType<typeof actions>; // ActionType 를 사용하여 모든 액션 객체들의 타입을 준비해줄 수 있습니다
 
 
 // 3.리듀서를 작성합니다.
 // 리듀서에서는 state 와 함수의 반환값이 일치하도록 작성하세요.
-function counter(
-  state: CounterState = initialState,
-  action: CounterAction // 액션에서는 우리가 방금 만든 CounterAction 을 타입으로 설정
-): CounterState {
-  switch (action.type) {
-    case INCREASE: // case 라고 입력하고 Ctrl + Space 를 누르면 어떤 종류의 action.type들이 있는지 확인 할 수 있습니다.
-      return { count: state.count + 1 };
-    case DECREASE:
-      return { count: state.count - 1 };
-    case INCREASE_BY:
-      return { count: state.count + action.payload };
-    default:
-      return state;
-  }
-}
+// function counter(
+//   state: CounterState = initialState,
+//   action: CounterAction // 액션에서는 우리가 방금 만든 CounterAction 을 타입으로 설정
+// ): CounterState {
+//   switch (action.type) {
+//     case INCREASE: // case 라고 입력하고 Ctrl + Space 를 누르면 어떤 종류의 action.type들이 있는지 확인 할 수 있습니다.
+//       return { count: state.count + 1 };
+//     case DECREASE:
+//       return { count: state.count - 1 };
+//     case INCREASE_BY:
+//       return { count: state.count + action.payload };
+//     default:
+//       return state;
+//   }
+// }
+
+// 리듀서를 만듭니다 => 리팩토링 : 오브젝트 맵 형태
+// createReducer 는 리듀서를 쉽게 만들 수 있게 해주는 함수입니다.
+// Generics로 리듀서에서 관리할 상태, 그리고 리듀서에서 처리 할 모든 액션 객체들의 타입을 넣어야합니다
+// const counter = createReducer<CounterState, CounterAction>(initialState, {
+//     [INCREASE]: state => ({ count: state.count + 1 }), // 액션을 참조 할 필요 없으면 파라미터로 state 만 받아와도 됩니다
+//     [DECREASE]: state => ({ count: state.count - 1 }),
+//     [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload }) // 액션의 타입을 유추 할 수 있습니다.
+//   });
+  
+
+// 리듀서를 만듭니다 => 체이닝으로 리팩토링 => 이건 CounterAction 생략 가능
+// createReducer 는 리듀서를 쉽게 만들 수 있게 해주는 함수입니다.
+// Generics로 리듀서에서 관리할 상태, 그리고 리듀서에서 처리 할 모든 액션 객체들의 타입을 넣어야합니다
+const counter = createReducer<CounterState, CounterAction>(initialState)
+  .handleAction(increase, state => ({ count: state.count + 1 }))
+  .handleAction(decrease, state => ({ count: state.count - 1 }))
+  .handleAction(increaseBy, (state, action) => ({
+    count: state.count + action.payload
+  }));
 
 export default counter;
--- typescript-react-redux-tutorial/src/modules/todos.ts
+++ typescript-react-redux-tutorial/src/modules/todos.ts
@@ -1,7 +1,8 @@
+import {deprecated, createAction, ActionType, createReducer} from 'typesafe-actions';
 // 액션 타입 선언
 const ADD_TODO = 'todos/ADD_TODO' as const;
-const TOGGLE_TODO = 'todos/TOGGLE_TODO' as const; // 할일을 껏다켰다
-const REMOVE_TODO = 'todos/REMOVE_TODO' as const; // 제거
+const TOGGLE_TODO = 'todos/TOGGLE_TODO'; // 할일을 껏다켰다
+const REMOVE_TODO = 'todos/REMOVE_TODO'; // 제거
 
 let nextId = 1; // 새로운 항목을 추가 할 때 사용 할 고유 ID 값
 
@@ -14,21 +15,40 @@ export const addTodo = (text: string) => ({
   }
 });
 
-export const toggleTodo = (id: number) => ({
-  type: TOGGLE_TODO,
-  payload: id
-});
+// export const toggleTodo = (id: number) => ({
+//   type: TOGGLE_TODO,
+//   payload: id
+// });
 
-export const removeTodo = (id: number) => ({
-  type: REMOVE_TODO,
-  payload: id
-});
+// export const removeTodo = (id: number) => ({
+//   type: REMOVE_TODO,
+//   payload: id
+// });
+
+//리팩토링
+// 이 액션 생성 함수의 경우엔 파라미터를 기반하여 커스터마이징된 payload를 설정해주므로, createAction 이라는 함수를 사용합니다.
+// export const addTodo = createAction(ADD_TODO, action => (text: string) =>// 여기서 action은 액션 객체를 만드는 함수입니다
+//   action({
+//     id: nextId++,
+//     text
+//   })
+// );
+export const toggleTodo = deprecated.createStandardAction(TOGGLE_TODO)<number>();// number을 payload로 갖고 온다
+export const removeTodo = deprecated.createStandardAction(REMOVE_TODO)<number>();
+
+// 모든 액션 객체들에 대한 타입 준비 => 리팩토링
+const actions = {
+    addTodo,
+    toggleTodo,
+    removeTodo
+};
+type TodosAction = ActionType<typeof actions>;
 
 // 모든 액션 객체들에 대한 타입 준비
-type TodosAction =
-  | ReturnType<typeof addTodo>
-  | ReturnType<typeof toggleTodo>
-  | ReturnType<typeof removeTodo>;
+// type TodosAction =
+//   | ReturnType<typeof addTodo>
+//   | ReturnType<typeof toggleTodo>
+//   | ReturnType<typeof removeTodo>;
 
 // 상태에서 사용 할 할 일 항목 데이터 타입 정의 => 컴포넌트에서도 쓸거라 export
 export type Todo = {
@@ -44,28 +64,42 @@ export type TodosState = Todo[];
 const initialState: TodosState = [];
 
 // 리듀서 작성
-function todos(
-  state: TodosState = initialState,
-  action: TodosAction
-): TodosState {
-  switch (action.type) {
-    case ADD_TODO:
-      return state.concat({
-        id: action.payload.id,// action.payload 객체 안의 값이 모두 유추됩니다.
-        text: action.payload.text,
+// function todos(
+//   state: TodosState = initialState,
+//   action: TodosAction
+// ): TodosState {
+//   switch (action.type) {
+//     case ADD_TODO:
+//       return state.concat({
+//         id: action.payload.id,// action.payload 객체 안의 값이 모두 유추됩니다.
+//         text: action.payload.text,
+//         done: false
+//       });
+//     case TOGGLE_TODO:
+//       return state.map(todo =>
+//         // payload 가 number 인 것이 유추됩니다.
+//         todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+//       );
+//     case REMOVE_TODO:
+//       // payload 가 number 인 것이 유추됩니다.
+//       return state.filter(todo => todo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// }
+
+//리듀서 작성 리팩토링
+const todos = createReducer<TodosState, TodosAction>(initialState, {
+    [ADD_TODO]: (state, action) =>
+      state.concat({
+        ...action.payload, // id, text 를 이 안에 넣기
         done: false
-      });
-    case TOGGLE_TODO:
-      return state.map(todo =>
-        // payload 가 number 인 것이 유추됩니다.
-        todo.id === action.payload ? { ...todo, done: !todo.done } : todo
-      );
-    case REMOVE_TODO:
-      // payload 가 number 인 것이 유추됩니다.
-      return state.filter(todo => todo.id !== action.payload);
-    default:
-      return state;
-  }
-}
+      }),   
+    // 비구조화 할당을 활용하여 payload 값의 이름을 바꿀 수 있음
+    [TOGGLE_TODO]: (state, { payload: id }) =>
+      state.map(todo => (todo.id === id ? { ...todo, done: !todo.done } : todo)),
+    [REMOVE_TODO]: (state, { payload: id }) =>
+      state.filter(todo => todo.id !== id)
+  });
 
 export default todos;

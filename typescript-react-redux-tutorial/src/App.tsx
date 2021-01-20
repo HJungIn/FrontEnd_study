1f41ff61b171657b105dbdd7aedc2b613c435c32
HJungIn <gjwjddls96@naver.com>
2021-01-20 22:51:20 +0900

    타입스크립트 5-2 : 타입스크립트와 리덕스 - 투두리스트

--- typescript-react-redux-tutorial/src/App.tsx
+++ typescript-react-redux-tutorial/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CounterContainer from './containers/CounterContainer';
+import TodoApp from './containers/TodoApp';
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       {/* 타입스크립트와 리덕스  */}
       {/* 카운터 - 기초 */}
       <CounterContainer />
+
+      {/* 투두리스트 만들기 */}
+      <TodoApp />
     </>
   );
 }

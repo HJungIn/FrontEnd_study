0c146eca7bd5d514ad3365922018b723734d326a
HJungIn <gjwjddls96@naver.com>
2021-01-26 18:51:55 +0900

    03. 기본 타입

---
+++ typescript-workshop/dist/03type.js
@@ -0,0 +1,19 @@
+let numValue;
+let stringValue;
+let booleanValue;
+let undefinedValue;
+let nullValue;
+let objValue; // objValue = {name: 'jay'};
+let symbolValue; // symbolValue = Symbol();
+let nameList; //배열
+nameList = ['1', '2', '3'];
+nameList.push('11');
+let user1;
+user1 = {
+    name: 'jay',
+    score: 30
+};
+let tuple1;
+let tuple2;
+tuple1 = [1, "hello"];
+tuple2 = [1, 2, 3];
---
+++ typescript-workshop/src/03type.ts
@@ -0,0 +1,22 @@
+let numValue: number;
+let stringValue: string;
+let booleanValue: boolean;
+let undefinedValue: undefined;
+let nullValue: null;
+let objValue: object; // objValue = {name: 'jay'};
+let symbolValue: symbol; // symbolValue = Symbol();
+
+let nameList: string[]; //배열
+nameList = ['1','2','3'];
+nameList.push('11');
+
+let user1: {name: string, score: number};
+user1 = {
+    name: 'jay',
+    score: 30
+}
+
+let tuple1: [number, string];
+let tuple2: [number, number, number];
+tuple1 = [1,"hello"];
+tuple2 = [1, 2, 3];
